e49dd94c0e786b73c0f8b4b3f2a469780ca5e0a0
saurabh-soni-dev <saurabh.soni6408@gmail.com>
2024-09-12 01:20:29 +0530

    Add two num

--- swapTwoNum.js
+++ swapTwoNum.js
@@ -1,5 +1,23 @@
 let a = 100;
 let b = 300;
 
-[a,b] = [b,a]
-console.log(a,b)
+// using destrut
+// [a,b] = [b,a]
+// console.log(a,b)
+
+// ------------------------------------------------
+
+// using temp
+let temp;
+// temp = a;
+// a = b;
+// b = temp;
+// console.log(a,b)
+
+// ------------------------------------------------
+
+// without using new var
+// a = a+b;
+// b = a-b;
+// a = a-b;
+// console.log(a,b)
